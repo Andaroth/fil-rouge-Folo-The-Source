09e879594cc807171f665b7de8c564d06f3c58fb
Anda <ax.fiolle@gmail.com>
2018-01-30 16:02:25 +0100

    wip

--- multipage/gallery.php
+++ multipage/gallery.php
@@ -11,21 +11,21 @@
 </head>
 <body class="purple darken-4">
   <?php include('topbar.php'); ?>
-  <div id="gallerycarousel" class="carousel carousel-slider minh-33vh">
-    <h2>Our actions in Guinea</h2>
-    <a class="carousel-item minh-33vh" href="#one!"><img class="minh-33vh" src="./img/inauguration.jpg" alt="well's inauguration"></a>
-    <a class="carousel-item minh-33vh" href="#two!"><img class="minh-33vh" src="./img/inauguration2.jpg" alt="well's inauguration"></a>
-    <a class="carousel-item minh-33vh" href="#three!"><img class="minh-33vh" src="./img/needwater.jpg" alt="people needs water"></a>
-    <a class="carousel-item minh-33vh" href="#four!"><img class="minh-33vh" src="./img/presentation.jpg" alt="well's presentation"></a>
-    <a class="carousel-item minh-33vh" href="#five!"><img class="minh-33vh" src="./img/water.jpg" alt="well's water"></a>
-    <a class="carousel-item minh-33vh" href="#five!"><img class="minh-33vh" src="./img/well.jpg" alt="well"></a>
-    <a class="carousel-item minh-33vh" href="#five!"><img class="minh-33vh" src="./img/well2.jpg" alt="well"></a>
-    <a class="carousel-item minh-33vh" href="#five!"><img class="minh-33vh" src="./img/well3.jpg" alt="well"></a>
-    <a class="carousel-item minh-33vh" href="#five!"><img class="minh-33vh" src="./img/people.jpg" alt="Guinea's people"></a>
+  <div class="row">
+    <div id="gallerycarousel" class="carousel carousel-slider minh-33vh">
+      <h2>Our actions in Guinea</h2>
+      <a class="carousel-item minh-33vh" href="#one!"><img class="minh-33vh" src="./img/inauguration.jpg" alt="well's inauguration"></a>
+      <a class="carousel-item minh-33vh" href="#two!"><img class="minh-33vh" src="./img/inauguration2.jpg" alt="well's inauguration"></a>
+      <a class="carousel-item minh-33vh" href="#three!"><img class="minh-33vh" src="./img/needwater.jpg" alt="people needs water"></a>
+      <a class="carousel-item minh-33vh" href="#four!"><img class="minh-33vh" src="./img/presentation.jpg" alt="well's presentation"></a>
+      <a class="carousel-item minh-33vh" href="#five!"><img class="minh-33vh" src="./img/water.jpg" alt="well's water"></a>
+      <a class="carousel-item minh-33vh" href="#five!"><img class="minh-33vh" src="./img/well.jpg" alt="well"></a>
+      <a class="carousel-item minh-33vh" href="#five!"><img class="minh-33vh" src="./img/well2.jpg" alt="well"></a>
+      <a class="carousel-item minh-33vh" href="#five!"><img class="minh-33vh" src="./img/well3.jpg" alt="well"></a>
+      <a class="carousel-item minh-33vh" href="#five!"><img class="minh-33vh" src="./img/people.jpg" alt="Guinea's people"></a>
+    </div>
   </div>
   <div class="container media">
-    <div class="row images">
-    </div>
     <div class="row videos">
       <h2>What we have done - Videos</h2>
       <iframe width="500" height="300" src="https://www.youtube.com/embed/5-txLZLQFyQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
