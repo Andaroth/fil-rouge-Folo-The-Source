7122fc7be3a482a8b64d21b2d06952a78aed0c1b
ilias-chergui <chergui.ilias@gmail.com>
2018-01-30 09:59:36 +0100

    upload svg & gallery

---
+++ multipage/gallery.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/font.css">
+  <link rel="stylesheet" href="./css/materialize.css">
+  <link rel="stylesheet" href="./css/gallery.css">
+  <title>Gallery</title>
+</head>
+<body class="purple darken-4">
+  <?php include('topbar.php'); ?>
+  <div class="container">
+    <div class="row">
+      <div class="col s4">
+        <img src="./img/inauguration.jpg" alt="well's inauguration">
+      </div>
+      <div class="col s4">
+        <img src="./img/inauguration2.jpg" alt="well's inauguration">
+      </div>
+      <div class="col s4">
+        <img src="./img/needwater.jpg" alt="people needs water">
+      </div>
+      <div class="col s4">
+        <img src="./img/presentation.jpg" alt="well's presentation">
+      </div>
+      <div class="col s4">
+        <img src="./img/water.jpg" alt="well's water">
+      </div>
+      <div class="col s4">
+        <img src="./img/well.jpg" alt="well">
+      </div>
+      <div class="col s4">
+        <img src="./img/well2.jpg" alt="well">
+      </div>
+      <div class="col s4">
+        <img src="./img/well3.jpg" alt="well">
+      </div>
+      <div class="col s4">
+        <img src="./img/people.jpg" alt="Guinea's people">
+      </div>
+    </div>
+  </div>
+  <?php include('footer.php'); ?>
+  <script src="./js/jquery.js"></script>
+  <script src="./js/materialize.js"></script>
+  <script src="./js/app.js"></script>
+</body>
+</html>
