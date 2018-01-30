7c5de54805abe7646092fd5531d2a7ae3fed35a5
Anda <ax.fiolle@gmail.com>
2018-01-30 14:58:58 +0100

    wip

--- multipage/index.php
+++ multipage/index.php
@@ -14,15 +14,40 @@
     
   </div>
   <div class="row">
-    
+    <div class="container">
+      <div class="col s1 m4 center-align">
+        <i class="material-icons medium">flag</i>
+        <h3>Country</h3>
+        <p>Guinea with it's varied landscape is located in West Africa. The rivers Niger, Gambia and Bafing arise in this Country. In the highlands grows the rainforest.</p>
+      </div>
+      <div class="col s1 m4 center-align">
+        <i class="material-icons medium">local_florist</i>
+        <h3>Culture</h3>
+        <p>The culture of Africa is varied and manifold. It is a product of the diverse populations that today inhabit the continent of Africa and the African Diaspora.</p>
+      </div>
+      <div class="col s1 m4 center-align">
+        <i class="material-icons medium">spa</i>
+        <h3>Tradition</h3>
+        <p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
+      </div>
+    </div>
   </div>
-  <div class="row">
-    
+  <div class="row purple darken-4 pt-25 pb-25 mb-0 orange-text">
+    <div class="container center-align">
+      <div class="col s6">
+        <img src="./img/well.jpg" class="responsive-img" alt="We need water">
+      </div>
+      <div class="col s6">
+        <h2>What we do</h2>
+        <p class="flow-text">Our mission is, to bring clean water and good working wells to Manfara, what means for people there, to stay healthy and alive. </p>
+        <p class="flow-text">African culture is expressed in its arts and crafts, folklore and religion, clothing, cuisine, music and languages.</p>
+      </div>
+    </div>
   </div>
   <div class="row orange darken-3 pt-25 pb-25">
     <div class="container">
       <h2>Further events</h2>
-      <div class="col s12 m6 l3">
+      <div class="col s12 l3">
         <div class="card">
           <div class="card-image homepcard">
             <img src="./img/hexagon.jpg" alt="The Hexagon">
@@ -39,7 +64,7 @@
           </div>
         </div>
       </div>
-      <div class="col s12 m6 l3">
+      <div class="col s12 l3">
         <div class="card">
           <div class="card-image homepcard">
             <img src="./img/sold_out.jpg" alt="Wedgewood Rooms">
@@ -56,7 +81,7 @@
           </div>
         </div>
       </div>
-      <div class="col s12 m6 l3">
+      <div class="col s12 l3">
         <div class="card">
           <div class="card-image homepcard">
             <img src="./img/yum.jpg" alt="The Junction">
@@ -73,7 +98,7 @@
           </div>
         </div>
       </div>
-      <div class="col s12 m6 l3">
+      <div class="col s12 l3">
         <div class="card">
           <div class="card-image homepcard">
             <img src="./img/glasgow.jpg" alt="The Glasgow Royal Concert Hall">
