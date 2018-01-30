6730630ca5d755e8a036079129eb73602c48b85c
Anda <ax.fiolle@gmail.com>
2018-01-30 15:40:51 +0100

    wip

--- multipage/index.php
+++ multipage/index.php
@@ -123,9 +123,106 @@
       </div>
     </div>
   </div>
-  <div class="row">
-    
+  <div class="row multimedia">
+ 
+    <div class="col s12 m4">
+ 
+      <div class="row">
+ 
+        <div class="card">
+ 
+          <div class="card-image">
+ 
+            <img height="300" src="./img/needwater.jpg" alt="people needs water">
+ 
+            <span class="card-title"  style="color:#fb8c00">Pictures</span>
+ 
+          </div>
+ 
+          <div class="card-content">
+ 
+            <p>Our actions in Guinea in pictures.</p>
+ 
+          </div>
+ 
+          <div class="card-action">
+ 
+            <a href="gallery.php">More Pictures</a>
+ 
+          </div>
+ 
+        </div>
+ 
+      </div>
+ 
+    </div>
+ 
+    <div class="col s12 m4">
+ 
+      <div class="row">
+ 
+        <div class="card">
+ 
+          <div class="card-image">
+ 
+            <img height="300" src="./img/nfalykouyate.jpg" alt="nfalykouyate singer">
+ 
+            <span class="card-title"  style="color:#fb8c00">Musics</span>
+ 
+          </div>
+ 
+          <div class="card-content">
+ 
+            <p>Some music from our Youtube account.</p>
+ 
+          </div>
+ 
+          <div class="card-action">
+ 
+            <a href="gallery.php">More Musics</a>
+ 
+          </div>
+ 
+        </div>
+ 
+      </div>
+ 
+    </div>
+ 
+    <div class="col s12 m4">
+ 
+      <div class="row">
+ 
+        <div class="card">
+ 
+          <div class="card-image">
+ 
+            <img height="300" src="./img/inauguration.jpg" alt="well's inauguration">
+ 
+            <span class="card-title"  style="color:#fb8c00">Videos</span>
+ 
+          </div>
+ 
+          <div class="card-content">
+ 
+            <p>What we have done in Guinea.</p>
+ 
+          </div>
+ 
+          <div class="card-action">
+ 
+            <a href="gallery.php">More Videos</a>
+ 
+          </div>
+ 
+        </div>
+ 
+      </div>
+ 
+    </div>
+    </div>
   </div>
+ 
   <?php include('footer.php'); ?> 
   <script src="./js/jquery.js"></script>
   <script src="./js/materialize.js"></script>
