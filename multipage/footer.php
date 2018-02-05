0b73f31ebb48f5a77346f8f7774476f9be7f0478
Anda <ax.fiolle@gmail.com>
2018-02-05 09:20:33 +0100

    Auto stash before merge of "Axel" and "origin/ilias"

--- multipage/footer.php
+++ multipage/footer.php
@@ -4,32 +4,32 @@
     <div class="row">
       <div class="col s2">
         <a href="http://www.facebook.com/folothesource">
-          <img width="50px" height="50px" src="./img/icons/facebook.svg" alt="facebook icon">
+          <img width="50px" height="50px" src="./img/icons/facebook.svg" alt="facebook icon" width="15">
         </a>
       </div>
       <div class="col s2">
         <a href="http://twitter.com/namungroup">
-          <img width="50px" height="50px" src="./img/icons/twitter.svg" alt="twitter icon">
+          <img width="50px" height="50px" src="./img/icons/twitter.svg" alt="twitter icon" width="15">
         </a>
       </div>
       <div class="col s2">
         <a href="https://www.youtube.com/playlist?list=PLJdqS1pVwcEoUKy7zuJ0nC_iXgcVz5wUc">
-          <img width="50px" height="50px" src="./img/icons/youtube.svg" alt="youtube icon">
+          <img width="50px" height="50px" src="./img/icons/youtube.svg" alt="youtube icon" width="15">
         </a>
       </div>
       <div class="col s2">
         <a href="https://www.flickr.com/photos/namungroup/albums/72157649505355783">
-          <img width="50px" height="50px" src="./img/icons/flickr.svg" alt="flickr icon">
+          <img width="50px" height="50px" src="./img/icons/flickr.svg" alt="flickr icon" width="15">
         </a>
       </div>
       <div class="col s2">
         <a href="skype: namun.management">
-          <img width="50px" height="50px" src="./img/icons/skype.svg" alt="skype icon">
+          <img width="50px" height="50px" src="./img/icons/skype.svg" alt="skype icon" width="15">
         </a>
       </div>
       <div class="col s2">
         <a href="http://folothesource.com/feed/">
-          <img width="50px" height="50px" src="./img/icons/rss.svg" alt="rss icon">
+          <img width="50px" height="50px" src="./img/icons/rss.svg" alt="rss icon" width="15">
         </a>
       </div>
       <br><br><br>
